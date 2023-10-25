27df3ad82ac70cd12514c9b6159d168e27d6be5c
Jani Eväkallio <jani.evakallio@gmail.com>
2023-10-25 20:51:50 +0100

    Fix css

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <div className="flex w-screen h-screen justify-start">
-          <main className="mx-auto w-screen md:w-2/3 xl:w-1/2 md:h-auto p-4 md:p-8 pb-32">
+          <main className="mx-auto md:my-auto w-screen md:w-2/3 xl:w-1/2 md:h-auto p-4 md:p-8 pb-32">
             <div className="relative bg-white w-full h-full md:h-auto p-8 md:rounded-xl md:shadow-xl">
               {children}
             </div>
